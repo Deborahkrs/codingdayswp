05f72df634a59b9a4cb04c0220d149bdb543c79d
Deborah Kreis <Deborah@MacBook-Air-de-Deborah.local>
2016-10-09 16:59:39 +0200

    Ajout du fichier index.php

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php get_header(); ?>
 
-<h1>Hello Wonderful World</h1>
+<section class="section1">
+  <div class="photocover"></div>
+  <article class="textcover">
+    <h1>Transformez votre idée en une entreprise avec des clients !</h1>
+    <p> Le premier accélérateur pour startups early-stage fondées par les étudiants et jeunes diplômés</p>
+    <a class="bouton" href="http://theschoolab.com/">Candidater</a>
+  </article>
+</section>
+<section class="benefices">
+  <article>
+    <i class="fa fa-home maison"  aria-hidden="true"></i>
+    <h3> Un écosystème </h3>
+    <p> 1800 mètres carrés dédiés à l'innovation, en plein coeur du sentier.</p>
+  </article>
+  <article>
+    <i class="fa fa-rocket fusee" aria-hidden="true"></i>
+    <h3> 15 startups </h3>
+    <p> Une promo soudée et du partage de compétences</p>
+  </article>
+  <article>
+    <i class="fa fa-flask expert" aria-hidden="true"></i>
+    <h3> Un réseau</h3>
+    <p>100+ mentors et experts à disposition</p>
+  </article>
+</section>
+<section class="partenaires">
+    <h2> Ils nous font confiance </h2>
+    <img class="logo_entreprises" src="trustees.png">
+</section>
 
 <?php get_footer(); ?>
