dd7c0c5885f1af25988f7d1ae3f4d3723e1d5a27
Deborah Kreis <Deborah@MacBook-Air-de-Deborah.local>
2016-10-09 16:28:07 +0200

    Mise en ligne du site

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
     <footer>
       <!-- Vous pouvez mettre ici toutes les infos que vous voulez retrouver dans le footer de toutes vos pages  -->
+    <p class="remerciements"> Fait avec amour par Romain et Debo</p>
+    <p>COPYRIGHT © 2016 SCHOOLAB TOUS DROITS RÉSERVÉS</P>
     </footer>
     <?php wp_footer(); ?>
   </body>
--- header.php
+++ header.php
@@ -16,12 +16,27 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">``
 	<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.min.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
 </head>
 <body>
-	<header class="header">
+	<header>
 		<!-- Vous pouvez mettre ici toutes les infos que vous voulez retrouver dans le header de toutes vos pages  -->
+		<img src="logo-starter-2016-02.png" alt="Logo Starter" id="starter">
+		<nav class="menu">
+			<ul>
+				<li class="categorie">
+					<a href="http://starter.theschoolab.com/starter/">Le programme</a>
+				</li>
+				<li class="categorie">
+					<a href="http://starter.theschoolab.com/les-startups/">Les startups</a>
+				</li>
+				<li class="categorie">
+					<a id="mentors" href="http://starter.theschoolab.com/les-mentors/"> Mentors et Experts</a>
+				</li>
+			</ul>
+		</nav>
 	</header>
